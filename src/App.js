e7ce590a2e398947082ecd5330223ddfd10b5eb2
Saeid Ostad <saostad2@gmail.com>
2019-06-30 14:36:17 -0400

    added prettier and eslint

---
+++ src/App.js
@@ -0,0 +1,8 @@
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!")
+  );
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
